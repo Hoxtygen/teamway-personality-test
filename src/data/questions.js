f6f813c576f0ca74cdba250acb50a1757fa0fa11
Wasiu Idowu <hoxtygen@live.com>
2023-01-27 10:24:15 +0100

    add questions and static data

---
+++ src/data/questions.js
@@ -0,0 +1,123 @@
+export const questions = [
+	{
+	  name: "question-1",
+	  text: "You’re really busy at work and a colleague is telling you their life story and personal woes. You: ",
+	  options: [
+		{
+		  score: 1,
+		  option: "A",
+		  id: 1,
+		  text: "Don’t dare to interrupt them ",
+		},
+		{
+		  score: 2,
+		  option: "B",
+		  id: 2,
+		  text: "Listen, but with only with half an ear ",
+		},
+		{
+		  score: 3,
+		  option: "C",
+		  id: 3,
+		  text: "Interrupt and explain that you are really busy at the moment ",
+		},
+	  ],
+	},
+	{
+	  name: "question-2",
+	  text: "You’ve been sitting in the doctor’s waiting room for more than 25 minutes. You: ",
+	  options: [
+		{
+		  score: 1,
+		  option: "A",
+		  id: 1,
+		  text: "Look at your watch every two minutes ",
+		},
+		{
+		  score: 2,
+		  option: "B",
+		  id: 2,
+		  text: "Explain to other equally impatient people in the room that the doctor is always running late ",
+		},
+		{
+		  score: 3,
+		  option: "C",
+		  id: 3,
+		  text: "Complain in a loud voice, while tapping your foot impatiently ",
+		},
+	  ],
+	},
+	{
+	  name: "question-3",
+	  text: "You’re having an animated discussion with a colleague regarding a project that you’re in charge of. You: ",
+	  options: [
+		{
+		  score: 1,
+		  option: "A",
+		  id: 1,
+		  text: "Don’t dare contradict them ",
+		},
+		{
+		  score: 2,
+		  option: "B",
+		  id: 2,
+		  text: "Defend your own point of view, tooth and nail ",
+		},
+		{
+		  score: 3,
+		  option: "C",
+		  id: 3,
+		  text: "Continuously interrupt your colleague ",
+		},
+	  ],
+	},
+	{
+	  name: "question-4",
+	  text: "You are taking part in a guided tour of a museum. You: ",
+	  options: [
+		{
+		  score: 1,
+		  option: "A",
+		  id: 1,
+		  text: "Are a bit too far towards the back so don’t really hear what the guide is saying ",
+		},
+		{
+		  score: 2,
+		  option: "B",
+		  id: 2,
+		  text: "Make sure that everyone is able to hear properly ",
+		},
+		{
+		  score: 3,
+		  option: "C",
+		  id: 3,
+		  text: "Are right up the front, adding your own comments in a loud voice ",
+		},
+	  ],
+	},
+	{
+	  name: "question-5",
+	  text: "During dinner parties at your home, you have a hard time with people who: ",
+	  options: [
+		{
+		  score: 1,
+		  option: "A",
+		  id: 1,
+		  text: "Ask you to tell a story in front of everyone else ",
+		},
+		{
+		  score: 2,
+		  option: "B",
+		  id: 2,
+		  text: "Hang around you all evening ",
+		},
+		{
+		  score: 3,
+		  option: "C",
+		  id: 3,
+		  text: "Always drag the conversation back to themselves ",
+		},
+	  ],
+	},
+  ];
+  
